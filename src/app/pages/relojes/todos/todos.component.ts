fe5435cee1316413836ee26c8fb2ce077a767b5d
gonzaloseg <gonzaloseg05@gmail.com>
2025-02-13 18:23:57 +0100

    toma q toma los relojitos

--- src/app/pages/relojes/todos/todos.component.ts
+++ src/app/pages/relojes/todos/todos.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
-  productos = [
-    { id: 1, nombre: 'Reloj Deportivo A', categoria: 'Deportivos' },
-    { id: 2, nombre: 'Reloj Clásico B', categoria: 'Clásicos' },
-    { id: 3, nombre: 'Reloj Deportivo C', categoria: 'Deportivos' },
-    { id: 4, nombre: 'Reloj Clásico D', categoria: 'Clásicos' },
-    { id: 5, nombre: 'FLK3453', precio: 700, imagen: '/imagenes/Captura de pantalla 2025-02-13 120619.png'}
-  ];
+
 }
