55bbdaecaec3f79270268cea9600545691ec636c
gonzaloseg <gonzaloseg05@gmail.com>
2025-02-20 14:11:23 +0100

    ya hemos puesto bonito el contacta

--- src/app/pages/contacta/contacta.component.ts
+++ src/app/pages/contacta/contacta.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 
 @Component({
   selector: 'app-contacta',
   templateUrl: './contacta.component.html',
-  styleUrl: './contacta.component.css'
+  styleUrls: ['./contacta.component.css']
 })
 export class ContactaComponent {
-  
+  model: any = {};
+
+  enviar(form: NgForm) {
+    if (form.valid) {
+      alert('Su mensaje se ha enviado correctamente');
+      form.resetForm(); // Resetea el formulario tras envío exitoso
+    }
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 
 
 
@@ -27,7 +28,8 @@ import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.co
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule 
   ],
   exports:[
     ContactaComponent,
